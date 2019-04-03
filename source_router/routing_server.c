5dbc1e520c14798f6540b5af65a36ea426804c96
christine <christine.winge@gmail.com>
2019-04-03 13:57:56 +0200

    Opprydning. Virker ish

--- source_router/routing_server.c
+++ source_router/routing_server.c
@@ -12,6 +12,31 @@
 
 int BASEPORT;
 int BACKLOG_SIZE = 10;
+int MAX_NUM_CLIENTS;
+
+char RESPONSE_SUCCESS[] = "Thank you for connecting!\n";
+char RESPONSE_FULL[] = "Client list is full :(\n";
+struct client *clients;
+
+struct client {
+    int fd;
+    char *ip;
+    unsigned short port;
+    char *name;
+    int number_of_neighbors;
+};
+
+void remove_client(int fd, struct client *clients) {
+    int i;
+
+    for (i = 0; i < MAX_NUM_CLIENTS; ++i) {
+        if (clients[i].fd == fd) {
+            clients[i].fd = -1;
+            free(clients[i].ip);
+            return;
+        }
+    }
+}
 
 int create_server_socket() {
     int ret, yes, server_socket = 1;
@@ -47,67 +72,157 @@ int create_server_socket() {
     return server_socket;
 }
 
-void run_server(int socket_listener) {
-    fd_set fds, read_fds;
-    char* buf;
+int add_client(int socket, struct sockaddr_in *client_addr, socklen_t addrlen, struct client clients[]) {
+    char *ip_buffer = malloc(16);
+    int i;
+
+
+    if (!inet_ntop(client_addr->sin_family, &(client_addr->sin_addr), ip_buffer, addrlen)) {
+        perror("inet_ntop");
+        strcpy(ip_buffer, "N/A");
+    }
+
+
+    for (i = 0; i < MAX_NUM_CLIENTS; ++i) {
+        /* We have capacity to handle this client*/
+        if (clients[i].fd == -1) {
+            clients[i].fd = socket;
+            clients[i].ip = ip_buffer;
+            clients[i].port = ntohs(client_addr->sin_port);
+
+            write(socket, RESPONSE_SUCCESS, strlen(RESPONSE_SUCCESS));
+            return 0;
+        }
+    }
+
+    /* We don't have capacity to handle this client*/
+    write(socket, RESPONSE_FULL, strlen(RESPONSE_FULL));
+    free(ip_buffer);
+    close(socket);
+    return 1;
+}
+
+int accept_new_client(int socket_listener, struct client *clients) {
     struct sockaddr_in client_addr;
     socklen_t addrlen = sizeof(struct sockaddr_in);
 
+    int client_socket = accept(socket_listener,
+        (struct sockaddr*)&client_addr,
+        &addrlen);
+    if (client_socket == -1) {
+        perror("accept");
+        return -1;
+    }
+
+    if (add_client(client_socket, &client_addr, addrlen, clients)) {
+        return -1;
+    }
+    return client_socket;
+}
+
+void receive_from_client(int client_socket, struct client *clients) {
+    printf("Receiving from client with socket %d\n", client_socket);
     int max_size_recv = 1000;
     int recv_bytes;
-    
-    FD_ZERO(&fds);
-    FD_SET(socket_listener, &fds);
 
-    char exit[] = "exit";
+    int number;
+    recv_bytes = recv(client_socket, &number, sizeof(int), 0);
 
+    printf("%d\n", number);
+    char *name = malloc(sizeof(char) * max_size_recv);
+    recv_bytes = recv(client_socket, name, max_size_recv, 0);
+    name[recv_bytes] = '\0';
+    printf("From connection: %s\n", name);
 
-    while(1) {
-        fds = read_fds;
-        printf("Hit men ikke lenger 0\n");
-
-        /*if (select(socket_listener+1, &fds, NULL, NULL, NULL) == -1) {
-            perror("select");
-            return;
-        }*/
-
-        printf("Hit men ikke lenger 1\n");
-        FD_SET(socket_listener, &fds);
-
-        if (FD_ISSET(socket_listener, &fds)) {
-            printf("Hit men ikke lenger 2\n");
-
-            int client_socket = accept(socket_listener, (struct sockaddr*)&client_addr, &addrlen);
-
-            printf("Client socket: %d\n", client_socket);
-
-            if (client_socket == -1) {
-                perror("accept");
-                return;
-            }
-
-            printf("Press enter to continue\n");
-
-
-            buf = malloc(sizeof(char) * max_size_recv+1);
-
-            do {
-                recv_bytes = recv(client_socket, buf, max_size_recv-1, 0);
-                buf[recv_bytes-2] = '\0';
-
-            } while (strcmp(buf, exit) != 0);
-
+    for (int j = 0; j < MAX_NUM_CLIENTS; j++) {
+        if (clients[j].fd == client_socket) {
+            clients[j].number_of_neighbors = number;
+            clients[j].name = name;
         }
     }
 }
 
+int go_through_fds(int socket_listener, fd_set *read_fds, fd_set *fds, int *largest_fd, struct client *clients, int *number_of_clients_added) {
+        /* Go through all possible file descriptors */
+    for (int i = 0; i <= *largest_fd; ++i) {
+        printf("Current socket: %d\n", i);
+        if (FD_ISSET(i, read_fds)) {
+            if (i == socket_listener) {
+                /* Accept new client */
+                int client_socket = accept_new_client(socket_listener, clients);
+
+                if (client_socket == -1) {
+                    perror("accept or add");
+                    continue;
+                }
+                if (client_socket > *largest_fd) {
+                    *largest_fd = client_socket;
+                }
+
+                FD_SET(client_socket, fds);
+            } 
+            else {
+                /* Receive from existing client */
+                receive_from_client(i, clients);
+                (*number_of_clients_added)++;
+                printf("%d\n", *number_of_clients_added);
+                if (*number_of_clients_added == MAX_NUM_CLIENTS) {
+                    return 0;
+                }
+            }
+        }
+    }
+    return 1;
+}
+
+struct client* run_server(int socket_listener) {
+    fd_set fds, read_fds;
+    int largest_fd = socket_listener;
+    int number_of_clients_added = 0;
+
+    
+    FD_ZERO(&fds);
+    FD_SET(socket_listener, &fds);
+
+    struct client *clients = malloc(sizeof(struct client) * MAX_NUM_CLIENTS);
+
+    for (int i = 0; i < MAX_NUM_CLIENTS; ++i) {
+        clients[i].fd = -1;
+    }
+
+    while (1) {
+        printf("While");
+        read_fds = fds;
+        if (select(largest_fd+1, &read_fds, NULL, NULL, NULL) == -1) {
+            perror("select");
+            return NULL;
+        }
+        /* Go through all possible file descriptors */
+        int ret = go_through_fds(socket_listener, &read_fds, &fds, &largest_fd, clients, &number_of_clients_added);
+        if (ret == 0) {
+            printf("All clients added!\n");
+            return clients;
+        }
+    }
+    return NULL;
+}
+
+
+
 int main(int argc, char *argv[]) {
-    assert(argc == 2);
+    assert(argc == 3);
 
     BASEPORT = atoi(argv[1]);
+    MAX_NUM_CLIENTS = atoi(argv[2]);
 
     int server_socket = create_server_socket();
 
-    run_server(server_socket);
+    clients = run_server(server_socket);
+    assert(clients != NULL);
+
+    for (int i = 0; i < MAX_NUM_CLIENTS; i++) {
+        struct client current = clients[i];
+        printf("Name: %s\nNumber of neighbors: %d\n\n", current.name, current.number_of_neighbors);
+    }
     return 0;
 }
